8ce16d004f25bac0769e26ff4a50f16b5d55ab62
JuanseFisaTS <142258092+JuanseFisaTS@users.noreply.github.com>
2023-08-17 16:42:24 -0500

    NUMERO DE DIGITOS

---
+++ NumeroDeDigitos/src/NumberUtils.java
@@ -0,0 +1,27 @@
+public class NumberUtils {
+
+    /**
+     * This method calculates and returns the number of digits in a given integer.
+     * If the number is negative, it returns -1.
+     * If the number is zero, it returns 1 as zero is considered to have one digit.
+     * @param number: an integer
+     * @return number of digits in number, or -1 if number is negative, or 1 if number is zero.
+     */
+    public int getNumberOfDigits(int number) {
+        // Write your code here
+         if(number < 0){
+             return -1;
+         }
+         if(number == 0){
+             return 1;
+         }
+         
+         int numDigitos = 1;
+         while (number>=10){
+             number = number /10;
+             numDigitos++;
+         }
+         return numDigitos;
+         
+    }
+}
